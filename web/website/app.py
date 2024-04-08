b866d3d4c93129cd3d69359f03823716ddac824e
Elijah Ochoa <elijahochoa8106@gmail.com>
2024-04-07 19:29:38 -0600

    added list of owned classes for teachers, list of joined classes for students, ability to join classes for students

--- web/website/app.py
+++ web/website/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 import uuid
 import firebase_admin
 import firebase
@@ -59,24 +60,37 @@ def get_status(id):
             return user_dict["is_teacher"] 
     return False
 def get_teacher_classes(id):
-    """get all classes a teacher owns"""
+    """Get all classes a teacher owns"""
     docs = db.collection("classrooms").stream()
     class_arr = []
     for classroom in docs:
         class_dict  = classroom.to_dict()
         if class_dict["ownerId"] == id:
-            class_arr.append(class_dict)
+            class_arr.append(class_dict["class_code"])
     return class_arr
-def get_student_classes(email):
+def get_student_classes( id, email):
     """Get all classes a student is a part of"""
-    docs = db.collection("classrooms").stream()
-    class_arr = []
-    for classroom in docs:
-        class_dict  = classroom.to_dict()
-        if email in class_dict["students"]:
-            class_arr.append(class_dict)
-    return class_arr
-        
+    print("student classes")
+    
+    results =  db.collection("classrooms").where(filter=FieldFilter("students", "array_contains", {"id": id ,"email"  : email})).stream()
+    enrolled_classes = []
+    for doc in results:
+        class_dict = doc.to_dict()
+        enrolled_classes.append(class_dict["class_code"]) 
+    return enrolled_classes
+def join_class(id, email, class_code):
+    """Given a student profile, and class code enter the student into a class """
+    print("joining class")
+    classes_ref = db.collection("classrooms")
+
+    query = classes_ref.where("class_code", "==", class_code).limit(1)
+    results = query.stream()
+    student_data = {"id": id, "email" : email}
+    for doc in results:
+        class_ref = doc.reference
+        class_ref.update({"students": firestore.ArrayUnion([student_data])})
+        return 200
+    return  401
 
 @app.route('/')
 def landing_page():
@@ -153,26 +167,26 @@ def dashboard_get():
         if is_teacher:
             class_data = get_teacher_classes(decoded_token["user_id"])
         else:
-            class_data = get_student_classes(decoded_token["email"])
+            class_data = get_student_classes(decoded_token["user_id"],decoded_token["email"])
+            print("student class data ", class_data)
         data = {
             'email': decoded_token['email'],
             'role' : "Teacher" if is_teacher else "Student"
         }
-        print("the data is ", data)
-        print(decoded_token)
     except Exception as e:
         print(e)
         return redirect(url_for('login'))
-    return render_template('dashboard.html', data=data, class_data=class_data)
+    print("class data ", class_data)
+    return render_template('dashboard.html', data=data, class_data=class_data, post_message = session.pop('message', None))
 
 @app.post('/dashboard')
 def create_or_join_classroom():
     """Add a classroom Document, each student joining is a collection which has 2 documents,
       assignment 1 and assignment 2. Assignment 1 and 2 both have field for completion"""
-    print(request.form["type"])
+    print("request type ", request.form["type"])
     if request.form["type"] == "add_class":
         #code for a teahcer to create  a class
-        if is_teacher:
+        if session["is_teacher"] == True:
             id = session["user"]["localId"]
             email = session["user"]["email"]
             is_teacher = get_status(id)
@@ -184,14 +198,14 @@ def create_or_join_classroom():
                 "students" : []
             }
             db.collection('classrooms').add(classroom)
-    elif request.form['type'] == "add_class":
+    elif request.form['type'] == "join_class":
         #code for a student to join a class
-        try:
-            class_code = request.form["class_code"]
-            email = session["user"]["localId"]
-            
-        except:
-            pass
+        class_code = request.form["class_code"]
+        email = session["user"]["email"]
+        student_id = session["user"]["localId"]
+        join_request = join_class(student_id,email,class_code)
+        if join_request == 401:
+            session["message"] = "Invlaid Class"
     return redirect(url_for('dashboard_get'))
 
 @app.route('/pig-part-1')

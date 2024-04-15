e6c5eed66ff5b0d84880164e2a9ad5c177ed6a66
Elijah Ochoa <elijahochoa8106@gmail.com>
2024-04-14 21:29:25 -0600

    added untracked files

---
+++ web/models/Classroom.py
@@ -0,0 +1,6 @@
+from models.User_Models import Teacher
+
+
+class Classroom():
+    def __init__(self, owner:Teacher ) -> None:
+        pass
---
+++ web/models/User_Models.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, email, password, isTeacher):
+        self.email = email
+        self.password = password
+        self.is_teacher = isTeacher

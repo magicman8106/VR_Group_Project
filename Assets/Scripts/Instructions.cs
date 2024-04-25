4541ee135cf9fdb9b2861730e85e04c410f33850
ctomasj <100461596+ctomasj@users.noreply.github.com>
2024-04-25 14:42:41 -0500

    Updated Act 2 Instructions

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -28,12 +28,13 @@ public class Instructions : MonoBehaviour
         "Objective:  To learn safe and proper dissection techniques and make an initial incision to access the pig's internal anatomy.",
         "Safety Reminders:\r\n\r\n    Always cut away from your body and fingers.\r\n    Use tools for their intended purpose.\r\n    Inform the instructor immediately in case of any cuts or injuries.",
         "Tips:\r\n\r\n    Dissect slowly and carefully.\r\n    Use the probe to initially separate tissues before cutting.\r\n    Keep your work area clean and organized.",
-        "Materials:\r\n\r\n    Dissecting tray\r\n    Preserved fetal pig\r\n    Dissection tools",
-        "Preparation:\r\n\r\n    Put on gloves and safety goggles.\r\n    Position the pig on its back in the dissecting tray.",
+        "Materials:\\r\\n\\r\\n    Dissecting tray\\r\\n    Preserved fetal pig\\r\\n    Dissection tools",
+        //"Preparation:\r\n\r\n    Put on gloves and safety goggles.\r\n    Position the pig on its back in the dissecting tray.",
         "Tool Selection:\r\n\r\n    Scalpel: Use for initial incisions into the skin. Handle with caution as blades are extremely sharp.\r\n    Scissors: Use for longer cuts and cutting through muscle tissue.\r\n    Forceps: Use for picking up and manipulating tissues.\r\n    Probe: Use for separating and exploring structures without cutting.",
-        "Initial Incision\r\n\r\n    Locate the umbilical cord: Find the cord on the mid-abdomen.\r\n    Shallow cut: Using the scalpel, make a shallow U-shaped incision around the umbilical cord, cutting through the skin only.\r\n    Deepening the cut: Switch to scissors and carefully extend the incision upward towards the neck and downward towards the hind legs. Cut through muscle layers as needed.",
-        "Flap Creation:\r\n\r\n    Gently lift the edges of the incision to create flaps of skin and muscle tissue.\r\n    Fold the flaps back to expose the underlying abdominal cavity.",
-        "Next Steps\r\n\r\n    Observe the exposed internal organs of the abdominal cavity.\r\n    Proceed with further dissections as directed by your instructor.\r\n    Important: Your instructor may have specific variations on the incision pattern or additional instructions. Always follow their guidance closely."
+        "Initial Incision\r\n\r\n    Follow along the marked areas with the Scalpel to cut open the pig.",
+        //"Flap Creation:\r\n\r\n    Gently lift the edges of the incision to create flaps of skin and muscle tissue.\r\n    Fold the flaps back to expose the underlying abdominal cavity.",
+        "Next:\r\n\r\n    Observe the exposed internal organs of the abdominal cavity.\r\n    Make an incision between the Trachea and Lung.",
+        "Next: Make an incision between the Vein and Heart."
         };
     private int currentPage;
 
